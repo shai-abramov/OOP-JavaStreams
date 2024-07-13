369f7df7e42a4cdf8c4c10a49fbf14c9e12906fb
shaya <shai.abramov@proton.me>
2024-07-13 10:08:54 +0300

    changes to Lambdas.java

--- JavaStreams/Lambdas.java
+++ JavaStreams/Lambdas.java
@@ -3,14 +3,26 @@ import java.util.*;
 public class Lambdas {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
-        processNumbers(list);
+        processNumbers(list, new IsEvenIntPredicate());
     }
 
-    private static void processNumbers(Iterable<Integer> numbers) {
+    private static void processNumbers(Iterable<Integer> numbers,
+                                       IntPredicate predicate) {
         for (int num : numbers) {
-            if (num % 2 == 0) {
+            if (predicate.acceptNumber(num)) {
                 System.out.print(num + " ");
             }
         }
+    }
+}
+
+interface IntPredicate {
+    boolean acceptNumber(int num);
+}
+
+class IsEvenIntPredicate implements IntPredicate {
+    @Override
+    public boolean acceptNumber(int num) {
+        return num % 2 == 0;
     }
 }
