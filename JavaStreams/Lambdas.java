5d6251b2c89322fd804d570fb17e57669a3b70a6
shaya <shai.abramov@proton.me>
2024-07-13 11:03:48 +0300

    changes to Lambdas.java

--- JavaStreams/Lambdas.java
+++ JavaStreams/Lambdas.java
@@ -3,15 +3,7 @@ import java.util.*;
 public class Lambdas {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
-
-        IntPredicate predicate = new IntPredicate() {
-            @Override
-            public boolean acceptNumber(int num) {
-                return false;
-            }
-        };
-
-        processNumbers(list, predicate);
+        processNumbers(list, num -> num % 5 == 0);
     }
 
 
